654bc802c497aef56abc2dfc84c319375be9cd7f
futao <futao@mysteel.com>
2022-08-08 17:31:38 +0800

    泛化调用

--- _1-base/consumer/src/main/java/com/futao/base/consumer/Main.java
+++ _1-base/consumer/src/main/java/com/futao/base/consumer/Main.java
@@ -1,16 +1,7 @@
 package com.futao.base.consumer;
 
-import com.futao.base.api.dto.UserAddress;
-import com.futao.base.api.service.GroupService;
-import com.futao.base.api.service.ValidService;
-import com.futao.base.consumer.service.OrderService;
-import com.futao.base.consumer.service.impl.GroupServiceConsumer;
-import com.futao.base.consumer.service.impl.LocalStubCallService;
+import org.apache.dubbo.rpc.service.GenericService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import javax.validation.ValidationException;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author futao
@@ -20,22 +11,26 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("classpath:dubbo-consumer.xml");
         classPathXmlApplicationContext.start();
-        System.out.println(classPathXmlApplicationContext.getBean(OrderService.class).initOrder("1"));
+        // System.out.println(classPathXmlApplicationContext.getBean(OrderService.class).initOrder("1"));
         // 每隔一秒循环调用，中途关闭ZK，会发现不会导致调用失败，因为消费者本地缓存了生产者的服务地址，所以ZK下线了也可以调到生产者
         // while (true) {
         //     classPathXmlApplicationContext.getBean(LocalStubCallService.class).call();
         //     TimeUnit.SECONDS.sleep(1);
         // }
 
-        classPathXmlApplicationContext.getBean(GroupServiceConsumer.class).ga();
-        classPathXmlApplicationContext.getBean(GroupServiceConsumer.class).gb();
+        // classPathXmlApplicationContext.getBean(GroupServiceConsumer.class).ga();
+        // classPathXmlApplicationContext.getBean(GroupServiceConsumer.class).gb();
 
 
-        try {
-            classPathXmlApplicationContext.getBean(ValidService.class).addUserAddress(new UserAddress());
-            classPathXmlApplicationContext.getBean(ValidService.class).updateUserAddress(new UserAddress());
-        } catch (Exception e) {
-            System.out.println(((ValidationException) e).getMessage());
-        }
+        // try {
+        //     classPathXmlApplicationContext.getBean(ValidService.class).addUserAddress(new UserAddress());
+        //     classPathXmlApplicationContext.getBean(ValidService.class).updateUserAddress(new UserAddress());
+        // } catch (Exception e) {
+        //     System.out.println(((ValidationException) e).getMessage());
+        // }
+
+        GenericService genericService = (GenericService) classPathXmlApplicationContext.getBean("userAddressServiceGeneric");
+        System.out.println(genericService.$invoke("byUserId", new String[]{String.class.getName()}, new Object[]{"1"}));
+
     }
 }
